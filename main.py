90d72ec52a38dd35fa16823046556039e6fd7106
Pontus Asp <pontus.asp@gmail.com>
2022-03-26 00:07:58 +0100

    finished bsod

--- main.py
+++ main.py
@@ -5,20 +5,32 @@ pygame.init()
 running=True
 width=800
 height=600
-Color_screen=(22,22,28)
-Color_blueflower=(0, 120, 215)
+screen_size = np.array([width, height])
+color_screen=(22,22,28)
+color_blueflower=(0, 120, 215)
+color_white=(255, 255, 255)
+color_black=(0, 0, 0)
 
 screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption('BLUEFLOWER')
 
 def load_tile(name):
-    return pygame.transform.scale(pygame.image.load(f'textures/tiles/{name}.png'), (64, 64))
+    return pygame.transform.scale(pygame.image.load(f'resources/tiles/{name}.png'), (64, 64))
 
 tiles = {
     "grass": load_tile('red-painter-man41')
 }
 
-screen_size = np.array([width, height])
+
+bg = pygame.image.load(r'resources/bsod.png')
+
+smileyFont = pygame.font.Font('resources/font/segoeui.ttf', 155)
+smiley = smileyFont.render(':(', True, color_white)
+smileyRect = smiley.get_rect()
+smileyRect.center = (3*width/10, 5*height/12)
+
+font = pygame.font.Font('resources/font/segoeui.ttf', 30)
+smallFont = pygame.font.Font('resources/font/segoeui.ttf', 14)
 
 seconds = lambda: pygame.time.get_ticks()/1000
 
@@ -35,9 +47,11 @@ x = 0
 y = 1
 
 scene = 1
+fullscreen = False
+fullscreen_time = seconds()
 
 def draw_scene1():
-    screen.fill(Color_screen)
+    screen.fill(color_screen)
     screen.blit(tiles["grass"], (x, y))
 
 def update_scene1():
@@ -46,18 +60,35 @@ def update_scene1():
         x += 1
     if key["a"]:
         x -= 1
+    if x > width:
+        scene = 3
 
 def draw_scene2():
-    screen.fill(Color_screen)
+    screen.fill(color_screen)
 
 def update_scene2():
     pass
 
 def draw_scene3():
-    screen.fill(Color_blueflower)
+    if seconds() - fullscreen_time < 1:
+        screen.fill(color_black)
+    else:
+        screen.fill(color_blueflower)
+        screen.blit(bg, (0,0))
 
 def update_scene3():
     pass
+
+def pre_update_scene3():
+    global fullscreen, bg, fullscreen_time
+    if not fullscreen:
+        fullscreen_time = seconds()
+        pygame.mouse.set_visible(False)
+        pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        display_info = pygame.display.Info()
+        bg = pygame.transform.scale(bg, (display_info.current_w, display_info.current_h)) 
+        fullscreen=True
+
 
 def draw():
     global x
@@ -67,13 +98,12 @@ def draw():
         draw_scene3()
     if scene == 3:
         draw_scene3()
-    pygame.draw.line(screen,Color_blueflower,(x,80),(130,100))
     pygame.display.flip()
     pygame.display.update()
 
 
 def handle_keyboard(keypress, keydown):
-    global key
+    global key, running
     if keypress == pygame.K_w:
         key["w"] = keydown
     if keypress == pygame.K_a:
@@ -82,6 +112,8 @@ def handle_keyboard(keypress, keydown):
         key["s"] = keydown
     if keypress == pygame.K_d:
         key["d"] = keydown
+    if keypress == pygame.K_ESCAPE:
+        running=False
 
 
 def handle_events():
@@ -102,9 +134,14 @@ def update():
     if scene == 3:
         update_scene3()
 
+def pre_update():
+    if scene == 3:
+        pre_update_scene3()
+
 
 def main():
     while running:
+        pre_update()
         draw()
         handle_events()
         update()
