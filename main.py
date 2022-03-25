6b0e6d18f47c7f075d61d6c6bda0eee8b9443550
Pontus Asp <pontus.asp@gmail.com>
2022-03-25 22:36:45 +0100

    got images and scaling working

--- main.py
+++ main.py
@@ -6,50 +6,108 @@ running=True
 width=800
 height=600
 Color_screen=(22,22,28)
-Color_line=(255,0,0)
-Color_line2=(255,255,0)
-Color_line3=(255,0,255)
+Color_blueflower=(0, 120, 215)
 
 screen=pygame.display.set_mode((width,height))
-screen.fill(Color_screen)
+pygame.display.set_caption('BLUEFLOWER')
 
-b0 = np.array([ 0,  0])
-b1 = np.array([ 0, 27])
-b2 = np.array([27, 27])
-b3 = np.array([27,  0])
+def load_tile(name):
+    return pygame.transform.scale(pygame.image.load(f'textures/tiles/{name}.png'), (64, 64))
+
+tiles = {
+    "grass": load_tile('red-painter-man41')
+}
 
 screen_size = np.array([width, height])
 
 seconds = lambda: pygame.time.get_ticks()/1000
 
+key = {
+    "w": False,
+    "a": False,
+    "s": False,
+    "d": False,
+}
+
 direction = 1
 
-def draw():
+x = 0
+y = 1
+
+scene = 1
+
+def draw_scene1():
     screen.fill(Color_screen)
-    if direction == 1:
-        pygame.draw.line(screen,Color_line,(seconds() * 100,80),(130,100))
-    elif direction == -1:
-        pygame.draw.line(screen,Color_line2,(seconds() * 100,80),(130,100))
-    elif direction == 2:
-        pygame.draw.line(screen,Color_line3,(seconds() * 100,80),(130,100))
+    screen.blit(tiles["grass"], (x, y))
+
+def update_scene1():
+    global x, scene
+    if key["d"]:
+        x += 1
+    if key["a"]:
+        x -= 1
+
+def draw_scene2():
+    screen.fill(Color_screen)
+
+def update_scene2():
+    pass
+
+def draw_scene3():
+    screen.fill(Color_blueflower)
+
+def update_scene3():
+    pass
+
+def draw():
+    global x
+    if scene == 1:
+        draw_scene1()
+    if scene == 2:
+        draw_scene3()
+    if scene == 3:
+        draw_scene3()
+    pygame.draw.line(screen,Color_blueflower,(x,80),(130,100))
     pygame.display.flip()
     pygame.display.update()
 
+
+def handle_keyboard(keypress, keydown):
+    global key
+    if keypress == pygame.K_w:
+        key["w"] = keydown
+    if keypress == pygame.K_a:
+        key["a"] = keydown
+    if keypress == pygame.K_s:
+        key["s"] = keydown
+    if keypress == pygame.K_d:
+        key["d"] = keydown
+
+
 def handle_events():
     global running, direction
-    for events in pygame.event.get():
-        if events.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running=False
-        if events.type == pygame.KEYDOWN:
-            direction = -1
-        if events.type == pygame.CONTROLLER_AXIS_LEFTY:
-            direction = 2
-        
+        if event.type == pygame.KEYDOWN:
+            handle_keyboard(event.key, True)
+        if event.type == pygame.KEYUP:
+            handle_keyboard(event.key, False)
+
+def update():
+    if scene == 1:
+        update_scene1()
+    if scene == 2:
+        update_scene2()
+    if scene == 3:
+        update_scene3()
+
 
 def main():
     while running:
         draw()
         handle_events()
+        update()
     pygame.quit()
     quit()
 
